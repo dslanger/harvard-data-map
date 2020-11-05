35bbdbebef06525ad4c30b3bf1be1d7ebba677dd
Dan Slanger <dslanger@gmail.com>
2020-11-05 14:42:03 -0700

    map shrinks, state fill color by year - map shrinks on initial click v1 [done] - color in state fill value based on death rate [done] - map data + fill colors change when user clicks on different years [done] - determine hover state fill color [done] - determine clicked state fill color [done]

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # harvard-data-map
+
 Created with CodeSandbox
+
+Requirements
+
+- map shrinks on initial click v1 [done]
+- color in state fill value based on death rate [done]
+- map data + fill colors change when user clicks on different years [done]
+- when user hovers over state, user sees tooltip with death rate (?) and prompt to click on state for more info (?)
+- when user clicks on state:
+- - user sees text and data visualizations for the given state, below the map
+- - a state specific hash is added to url / window.location
+
+- when user visits state specific hash url, user sees text and data visualizations for the given state, below the map
+
+ToDos
+
+- determine hover state fill color [done]
+- determine clicked state fill color [done]
+- refactor state data so each state's year key has separate key for both deaths and zone
--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-import "./styles.css";
+import "./styles.scss";
+import { mapGraphic } from "./map.js";
+import stateData from "./stateData.js";
+
+const map = document.querySelector(".map");
+
+const shrinkMap = (map) => {
+  map.addEventListener("click", function () {
+    this.classList.add("is-active");
+  });
+};
+
+const getZone = (year) => {
+  if (year <= 10) {
+    return 1;
+  } else if (year > 10 && year <= 20) {
+    return 2;
+  } else if (year > 20 && year <= 30) {
+    return 3;
+  } else if (year > 30 && year <= 40) {
+    return 4;
+  } else if (year > 40 && year <= 50) {
+    return 5;
+  } else {
+    return 0;
+  }
+};
+
+let year;
+
+const yearOptions = document.getElementsByClassName("year-option");
+const updateData = () => {
+  for (var i = 0; i < yearOptions.length; i++) {
+    if (yearOptions[i].checked) {
+      year = yearOptions[i].value;
+    }
+  }
+  stateData.forEach((state) => {
+    let path = document.getElementById(state.ID);
+    path.setAttribute("data-year", state[year]);
+    path.setAttribute("data-zone", getZone(state[year]));
+  });
+};
+
+for (var i = 0; i < yearOptions.length; i++) {
+  yearOptions[i].addEventListener("change", () => {
+    updateData();
+  });
+}
+
+map.innerHTML = mapGraphic;
+
+shrinkMap(map);
+
+updateData("2017");
+
+// stateData.forEach((state) => {
+//   const path = document.getElementById(state.ID);
+//   path.setAttribute("data-year", state[year]);
+//   path.classList.add(getYearClass(state[year]));
+// });
